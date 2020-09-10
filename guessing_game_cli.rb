b82f98ced6bc2a8afa5ea39aab5142f7a888532d
Erick Villeda <eeverickeev@gmail.com>
2020-09-10 15:47:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   current_num = rand(6) + 1
   prompt_user = "Guess a number between 1 and 6"
   user_input = gets.chomp
-  if user_input == 'exit'
+  if user_input.downcase == 'exit'
     puts "Goodbye!"
      elsif user_input != current_num
    puts "Sorry! The computer guessed #{current_num}."
